6ab32075ea96dfc610e00b253a04dec61c8b3c89
Empty17 <132923260+Empty17@users.noreply.github.com>
2023-08-28 15:57:59 -0300

    Add files via upload

---
+++ ADO2/src/UI.java
@@ -0,0 +1,17 @@
+public class UI {
+    public void boasVindas(){
+        System.out.println("*********************************************************");
+        System.out.println("SEJA BEM-VINDO AO SISTEMA DE JUROS");
+        System.out.println("Aqui o total da compra e o valor das parcelas \na partir da quantidade de " +
+                "vezes que você deseja PAGAR!");
+        System.out.println("*********************************************************");
+    }
+
+    public void extrato(Compra compra, Pagamento pagamento){
+        System.out.println("****************************");
+        System.out.println("EXTRATO DA COMPRA");
+        System.out.println("****************************");
+
+
+    }
+}
