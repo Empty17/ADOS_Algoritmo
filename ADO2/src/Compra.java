968b45e3fee93737a87e44132aefb3b1cd69d3e2
Empty17 <132923260+Empty17@users.noreply.github.com>
2023-08-29 17:12:21 -0300

    Add files via upload

---
+++ ADO2/src/Compra.java
@@ -0,0 +1,20 @@
+public class Compra {
+    private double compra;
+    private int parcelas;
+
+    public double getCompra() {
+        return compra;
+    }
+
+    public void setCompra(double compra) {
+        this.compra = compra;
+    }
+
+    public int getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
+}
