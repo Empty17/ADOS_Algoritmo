f882da551b69b43493a5c19834748f27f36a7e60
Empty17 <132923260+Empty17@users.noreply.github.com>
2023-08-29 16:51:04 -0300

    Add files via upload

---
+++ ADO2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        //Classes
+        UI ui = new UI();
+        Pagamento pagamento = new Pagamento();
+        Compra compra = new Compra();
+
+        ui.boasVindas();
+
+        System.out.println("Digite o valor da compra:");
+        compra.setCompra(ler.nextDouble());
+        System.out.println("Digite a quantidade de parcelas");
+        compra.setParcelas(ler.nextInt());
+
+
+        ui.extrato(compra,pagamento);
+
+    }
+}
