ea16e9cc65ef598e20f11e0e7a59ca1c999702ad
Empty17 <132923260+Empty17@users.noreply.github.com>
2023-08-31 15:18:04 -0300

    UI.java

--- ADO2/src/UI.java
+++ ADO2/src/UI.java
@@ -11,11 +11,11 @@ public class UI {
         System.out.println("\n\n****************************");
         System.out.println("EXTRATO DA COMPRA");
         System.out.println("****************************");
-        System.out.printf("VALOR DA COMPRA: %.2f\n", compra.getCompra());
+        System.out.printf("VALOR DA COMPRA:R$ %.2f\n", compra.getCompra());
         System.out.printf("QUANTIDADE DE PARCELAS: %d\n", compra.getParcelas());
         System.out.printf("JUROS: %.1f%%\n\n", pagamento.mostrarJuros(compra.getParcelas()));
-        System.out.printf("VALOR A SER PAGO: %.2f", pagamento.calcularTotal(compra));
-        System.out.printf("\nPARCELAS MENSAIS: %.2f", pagamento.calcularParcelas(pagamento.calcularTotal(compra),compra.getParcelas()));
+        System.out.printf("VALOR A SER PAGO:R$ %.2f", pagamento.calcularTotal(compra));
+        System.out.printf("\nPARCELAS MENSAIS:R$ %.2f", pagamento.calcularParcelas(pagamento.calcularTotal(compra),compra.getParcelas()));
         System.out.println("\n****************************");
 
 
